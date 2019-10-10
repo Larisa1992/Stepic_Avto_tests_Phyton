a5b7cf09852b3b7dda7f232735ef864d77433c52
Larisa <pelar1992@mail.ru>
2019-10-10 13:41:49 +0300

    add two new files

---
+++ Erroe_Message.py
@@ -0,0 +1,7 @@
+#Если значение выражения истинно,
+#то в консоли не должно появиться дополнительных сообщений (в днном случае Message for user)
+assert abs(-42)==-42, "Message for user"
+
+#Форматирование строк
+print("Let's count together! {}, then goes {}, and then {}".format("one", "two", "three"))
+
