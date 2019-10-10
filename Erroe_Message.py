b40cdd3b8a637378a80e58a05b374a37f21c0809
Larisa <pelar1992@mail.ru>
2019-10-10 16:02:45 +0300

    add

--- Erroe_Message.py
+++ Erroe_Message.py
@@ -5,3 +5,15 @@ assert abs(-42)==-42, "Message for user"
 #Форматирование строк
 print("Let's count together! {}, then goes {}, and then {}".format("one", "two", "three"))
 
+str1 = "one"
+str2 = "two"
+str3 = "third"
+print(f"Let's count together! {str1}, then goes {str2}, and then {str3}")
+
+
+def test_input_text(expected_result, actual_result):
+    # ваша реализация, напишите assert и сообщение об ошибке
+    assert expected_result == actual_result,"expected " + str(expected_result) + ", got " + str(actual_result)
+
+    def test_substring(full_string, substring):
+    assert substring in  full_string,"expected '" + str(substring) + "' to be substring of '" + str(full_string) + "'"
